0039accebde1cd978e037ecd8efaf916148eab61
Taylor Tobin <19tt94@gmail.com>
2019-02-16 11:05:15 -0800

    simple transition animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
 		<h1 class="heading">TT</h1>
 		<h3 class="heading">Developer</h3>
 		<button class="first-transition"
-			@click="firstTransition" 
-			:class="{'transition': transition}">
+			@touchstart="start"
+			:class="{'transition': expand}">
 		</button>
   </div>
 </template>
@@ -16,13 +16,20 @@ export default {
 	
 	data() {
 		return {
-			transition: false
+			expand: false
 		}
 	},
 
 	methods: {
-		firstTransition() {	
-			this.transition = true;		
+		start() {				
+			this.expand = true;
+			// animate button before transition
+			setTimeout(() => {
+				this.$router.push({ path: 'featured' });
+				this.transition = false;		
+			}, 2000);
+		},
+		go() {
 			// animate button before transition
 			setTimeout(() => {
 				this.$router.push({ path: 'featured' });
@@ -47,7 +54,8 @@ export default {
 	.first-transition {
 		position: absolute;
 		top: calc(50% - 20px);
-		left: calc(50% - 20px);
+		left: 0;
+		right: 0;
 		border: 2px solid $gold;
 		border-radius: 50%;
 		background: transparent;
@@ -59,20 +67,22 @@ export default {
 		&:before {
 			content: '';
 			position: absolute;
-			top: 0;
-			left: 0;
+			top: -2px;
+			left: -2px;
+			right: 0;
 			margin: 0 auto;
 			border: 2px solid $white;
 			border-radius: 50%;
-			width: 80px;
-			height: 80px;
-			transition: all ease 0.5s;
+			width: 100%;
+			height: 100%;
+			transform: scale(0);
+			transition: all ease 2.5s;
 		}
 	}
 
 	.transition {
-		&:after {
-			transform: scale(0.2);
+		&:before {
+			transform: scale(3);
 		}	
 	}
 }
