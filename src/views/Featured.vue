f2ca2c37a97bad1404fa845c81db73812ea01c87
Taylor <19tt94@gmail.com>
2019-02-22 14:21:32 -0800

    working project next button

--- src/views/Featured.vue
+++ src/views/Featured.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="featured">
     <div class="section"></div>
+    <div class="featured-wrapper">
+      <button class="next-project" @click="next()">Next</button>
+      
+      <div class="featured-info">
+        <h1>{{current_project.name}}</h1>
+        <p v-if="current_project.type">{{current_project.type}}</p>
+        <p v-if="current_project.name">{{current_project.name}}</p>
+        <a :href="current_project.link" v-if="current_project.link">Visit</a>
+      </div>     
 
-    <div class="featured-info">
-      <h1>{{projects[0].name}}</h1>
-      <p v-if="projects[0].type">{{projects[0].type}}</p>
-      <p v-if="projects[0].name">{{projects[0].name}}</p>
-      <a :href="projects[0].link" v-if="projects[0].link">Visit</a>
-    </div>
-
-    <div class="featured-slider">
-      <img :src="projects[0].slides[0]">
+      <div class="featured-slider">
+        <img :src="projects[0].slides[0]">
+      </div>
     </div>
   </div>
 </template>
@@ -18,9 +21,34 @@
 <script>
 export default {
   name: 'Featured',
+
+  computed: {
+    /** @returns index of selected project if it doesn't exceed the length */
+    current_project() {
+      return this.projects[this.project_index];
+    },
+    /** @returns length of projects */
+    length() {
+      return this.projects.length;
+    }
+  },
+
+  methods: {
+    /** @increments current project */
+    next() {      
+      if(this.project_index === (this.length - 1)) {
+        // reset index
+        this.project_index = 0;
+      } else {
+        this.project_index = this.project_index + 1;
+      }
+    }
+  },
   
   data() {
     return {
+      show: false,
+      project_index: 0,
       projects: [
         {
           "name": "Dreamworks Animation",
@@ -82,6 +110,24 @@ export default {
   position: relative;
   width: 100%;
   height: 100%;
+
+  &-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .next-project {
+    color: $black;
+    background: $gold;
+    padding: 0 1rem;
+    position: absolute;
+    width: 100%;
+    z-index: $front;
+  }
 
   &-info {
     width: 100%;
